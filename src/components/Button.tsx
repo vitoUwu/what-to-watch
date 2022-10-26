2ba9cfdf97be940e71f955b3c558f2b1c6bce032
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2022-10-26 00:25:33 -0400

    removing comments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,12 +12,10 @@ export function Button({ children, asChild, onClick }: ButtonProps) {
   const Component = asChild ? Slot : 'button'
 
   return (
-    // <div className='w-fit bg-gradient-to-r from-sky-500 to-violet-700 p-[2px] rounded active:from-sky-700 active:to-violet-900'>
     <GradientBorder>
-      <Component onClick={onClick} className='rounded px-4 py-1 bg-zinc-900 hover:bg-gradient-to-r from-sky-500 to-violet-700 active:from-sky-700 active:to-violet-900'>
+      <Component onClick={onClick} className='outline-none ring-2 focus:ring-zinc-200 rounded px-4 py-1 bg-zinc-900 hover:bg-gradient-to-r from-sky-500 to-violet-700 active:from-sky-700 active:to-violet-900'>
         {children}
       </Component>
     </GradientBorder>
-    // </div>
   )
 }
